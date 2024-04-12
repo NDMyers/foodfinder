71dd80bcac5d08535dfcbc87739fa15eab00dbbd
Nick Myers <ndm81001@gmail.com>
2024-04-11 19:29:36 -0700

    removed API path reroute from config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'https://maps.googleapis.com/:path*', // Proxy all requests to Google Places API
-            },
-        ];
-    },
+    
 };
 
 export default nextConfig;
